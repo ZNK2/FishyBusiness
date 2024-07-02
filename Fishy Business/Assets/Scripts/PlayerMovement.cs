5363e6af20c249eee19f0a2c5383767aed097f33
Guillaume Bravetti <guillaume.bravetti@epita.fr>
2024-07-02 23:13:25 +0200

    Cat animations

--- Fishy Business/Assets/Scripts/PlayerMovement.cs
+++ Fishy Business/Assets/Scripts/PlayerMovement.cs
@@ -6,34 +6,15 @@ public class PlayerControl : MonoBehaviour
     private float _speedX, _speedY;
     public float moveSpeed;
 
-    /*
-    private bool _isMoving;
-    private bool _facesRight = true;
-    private bool _facesLeft;
-    */
+    public Animator animator;
 
     void Update()
     {
         _speedX = Input.GetAxisRaw("Horizontal");
         _speedY = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(_speedX, _speedY).normalized * moveSpeed;
-
-        /*
-        if (_speedX != 0 || _speedY != 0)
-            _isMoving = true;
-        else
-            _isMoving = false;
-
-        if (_speedX > 0)
-        {
-            _facesRight = true;
-            _facesLeft = false;
-        }
-        else if (_speedX < 0)
-        {
-            _facesRight = false;
-            _facesLeft = true;
-        }
-        */
+        
+        animator.SetFloat("SpeedX", _speedX);
+        animator.SetFloat("SpeedY", _speedY);
     }
 }
